16a4caace76ab4edd2f4b42245ae6f86f4133ff9
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-05-05 16:00:54 +0545

    view binding:
    * applying view binding
    * showing toast
    * handling click handler on button

--- app/src/main/java/com/chaudharynabin/happybirthday/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin/happybirthday/MainActivity.kt
@@ -2,10 +2,26 @@ package com.chaudharynabin.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.chaudharynabin.happybirthday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+//    view binding is important concept
+//    https://www.youtube.com/watch?v=z0F2QTAKsWU
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(this.layoutInflater)
+        setContentView(this.binding.root)
+
+    }
+
+    fun createBirthdayCard(view: View) {
+        var name = this.binding.nameEditText.editableText
+
+        Toast.makeText(this,"Name: $name",Toast.LENGTH_SHORT).show()
+
     }
 }
---
+++ app/src/main/java/com/chaudharynabin/happybirthday/MainActivity.md
@@ -0,0 +1,10 @@
+# view binding
+https://android-developers.googleblog.com/2022/02/discontinuing-kotlin-synthetics-for-views.html
+
+* android-kotlin-extension is deprecated
+    * https://antonioleiva.com/kotlin-android-extensions/
+* instead use view biding or  Parcelize
+* but most modern is android jetpack
+    * Android's modern UI toolkit. Layouts built with **Compose** are declarative Kotlin code, 
+      eliminating the need to work with view references.
+      
