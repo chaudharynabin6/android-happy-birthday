7c6022c9857e3df486ccca77cadc8d78b3c78a1f
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-05-05 23:47:01 +0545

    changing image feature
    * using setImageResource

--- app/src/main/java/com/chaudharynabin/happybirthday/GreetingCard.kt
+++ app/src/main/java/com/chaudharynabin/happybirthday/GreetingCard.kt
@@ -2,6 +2,7 @@ package com.chaudharynabin.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.chaudharynabin.happybirthday.databinding.ActivityGreetingCardBinding
 
 class GreetingCard : AppCompatActivity() {
@@ -11,6 +12,9 @@ class GreetingCard : AppCompatActivity() {
     }
 //    binding
     private  lateinit var binding: ActivityGreetingCardBinding
+
+    private var array = arrayOf(R.drawable.frame_0,R.drawable.frame_1,R.drawable.frame_2,R.drawable.frame_3)
+    private  var index  = 0
 //    onCreate method
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,5 +25,28 @@ class GreetingCard : AppCompatActivity() {
         val name = this.intent.getStringExtra(NAME_EXTRAS)
 
         this.binding.happyBirthdayTextView.text  = name
+    }
+    private fun decreaseIndex(){
+        if(this.index == 0){
+            this.index = this.array.size - 1
+        }else{
+            this.index--
+        }
+    }
+    private fun increaseIndex(){
+        if(this.index == this.array.size -1 ){
+            this.index = 0
+        }
+        else{
+            this.index++
+        }
+    }
+    fun previousImage(view: View) {
+        decreaseIndex()
+        this.binding.frameImageView.setImageResource(this.array[index])
+    }
+    fun nextImage(view: View) {
+        increaseIndex()
+        this.binding.frameImageView.setImageResource(this.array[index])
     }
 }
